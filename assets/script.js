4d55d9e83cc59716c49fdb705bf1319fd929814e
mecuboi <matthewd.jusuf@gmail.com>
2022-08-26 11:39:17 +1000

    starting javascript var

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,9 @@ var prompt = {
     question: "This is the question"
     
 }
+
+var questionPrompt = getElementID(question);
+var choice1 = getElementID(q1a);
+var choice2 = getElementID(q2a);
+var choice3 = getElementID(q3a);
+var choice4 = getElementID(q4a);
