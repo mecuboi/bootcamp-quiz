e03941a4613981ce42a26abd86efbc641d2d2d81
mecuboi <matthewd.jusuf@gmail.com>
2022-08-26 11:48:01 +1000

    more JS

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,54 @@
-var prompt = [
+var questions = [
     {
-        question: 'this is the question?'
-        mcq1: 'a'
-        mcq2: 'b'
-        mcq3: 'c'
-        mcq4: 'd'
-        answer: 'a'
+        question: 'this is the question?',
+        mcq1: 'a',
+        mcq2: 'b',
+        mcq3: 'c',
+        mcq4: 'd',
+        answer: 'a',
     }
     {
-        question: 'this is the question?'
-        mcq1: 'a'
-        mcq2: 'b'
-        mcq3: 'c'
-        mcq4: 'd'
-        answer: 'b'
+        question: 'this is the question?',
+        mcq1: 'a',
+        mcq2: 'b',
+        mcq3: 'c',
+        mcq4: 'd',
+        answer: 'b',
     }
     {
-        question: 'this is the question?'
-        mcq1: 'a'
-        mcq2: 'b'
-        mcq3: 'c'
-        mcq4: 'd'
-        answer: 'b'
+        question: 'this is the question?',
+        mcq1: 'a',
+        mcq2: 'b',
+        mcq3: 'c',
+        mcq4: 'd',
+        answer: 'c',
     }
     {
-        question: 'this is the question?'
-        mcq1: 'a'
-        mcq2: 'b'
-        mcq3: 'c'
-        mcq4: 'd'
-        answer: 'c'
+        question: 'this is the question?',
+        mcq1: 'a',
+        mcq2: 'b',
+        mcq3: 'c',
+        mcq4: 'd',
+        answer: 'd',
     }
     {
-        question: 'this is the question?'
-        mcq1: 'a'
-        mcq2: 'b'
-        mcq3: 'c'
-        mcq4: 'd'
-        answer: 'd'
+        question: 'this is the question?',
+        mcq1: 'a',
+        mcq2: 'b',
+        mcq3: 'c',
+        mcq4: 'd',
+        answer: 'b',
     }
 ]
 
-var questionPrompt = getElementID(question);
-var choice1 = getElementID(q1a);
-var choice2 = getElementID(q2a);
-var choice3 = getElementID(q3a);
-var choice4 = getElementID(q4a);
+var questionPrompt = getElementID('question');
+var choice1 = getElementID('q1a');
+var choice2 = getElementID('q2a');
+var choice3 = getElementID('q3a)';
+var choice4 = getElementID('q4a');
 
 var score = 0
+
+var startButton = getElementID('start-button')
+
 
