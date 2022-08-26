faf2d1db5e600353ae35b9d8c90d2b103b75d17c
mecuboi <matthewd.jusuf@gmail.com>
2022-08-26 11:44:31 +1000

    adding questions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,51 @@
-var prompt = {
-    question: "This is the question"
-    
-}
+var prompt = [
+    {
+        question: 'this is the question?'
+        mcq1: 'a'
+        mcq2: 'b'
+        mcq3: 'c'
+        mcq4: 'd'
+        answer: 'a'
+    }
+    {
+        question: 'this is the question?'
+        mcq1: 'a'
+        mcq2: 'b'
+        mcq3: 'c'
+        mcq4: 'd'
+        answer: 'b'
+    }
+    {
+        question: 'this is the question?'
+        mcq1: 'a'
+        mcq2: 'b'
+        mcq3: 'c'
+        mcq4: 'd'
+        answer: 'b'
+    }
+    {
+        question: 'this is the question?'
+        mcq1: 'a'
+        mcq2: 'b'
+        mcq3: 'c'
+        mcq4: 'd'
+        answer: 'c'
+    }
+    {
+        question: 'this is the question?'
+        mcq1: 'a'
+        mcq2: 'b'
+        mcq3: 'c'
+        mcq4: 'd'
+        answer: 'd'
+    }
+]
 
 var questionPrompt = getElementID(question);
 var choice1 = getElementID(q1a);
 var choice2 = getElementID(q2a);
 var choice3 = getElementID(q3a);
 var choice4 = getElementID(q4a);
+
+var score = 0
+
